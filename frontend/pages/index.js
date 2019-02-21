0a25d758231a67786164de19259624f59c3bc812
myang <myopia92@gmail.com>
2019-02-21 15:38:51 -0500

    more clean ups

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,2 +1,11 @@
-const Home = () => <div>Home</div>;
+import Link from 'next/link';
+
+const Home = () => (
+    <div>
+        <h1>Home</h1>
+        <Link href="/test">
+            <a>test me</a>
+        </Link>
+    </div>
+);
 export default Home;
