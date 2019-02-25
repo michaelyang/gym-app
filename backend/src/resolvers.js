376d80aa2f2f891db972e461c44fdde65fdb5a88
Michael Yang <myang@Michael-Yang.cable.rcn.com>
2019-02-25 00:34:45 -0500

    adding initial user typedefs and simple registration

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -1,6 +1,30 @@
+const bcrypt = require("bcryptjs");
+
 const resolvers = {
   Query: {
-    currentUser: () => "Hello World"
+    currentUser(parent, args, ctx, info) {
+      return ctx.db.query.user(
+        { where: { id: "cjsjwl3zi4bgx0b86eo2yrofa" } },
+        info
+      );
+    }
+  },
+  Mutation: {
+    async register(parents, args, ctx, info) {
+      const { name, email, password } = args;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await ctx.db.mutation.createUser(
+        {
+          data: {
+            name,
+            email,
+            password: hashedPassword
+          }
+        },
+        info
+      );
+      return user;
+    }
   }
 };
 
--- backend/src/typeDefs.js
+++ backend/src/typeDefs.js
@@ -2,16 +2,19 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type User {
-    id: ID!
-    username: String!
+    id: ID! @unique
+    name: String!
+    email: String! @unique
+    password: String!
   }
 
   type Query {
     currentUser: User!
   }
 
-  #  type Mutation {
-  #  }
+  type Mutation {
+    register(name: String!, email: String!, password: String!): User!
+  }
 `;
 
 module.exports = typeDefs;
